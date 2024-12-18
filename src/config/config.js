57a617c0d67c37cff78726b8468cf0c49aeb409a
canopus <vivekc19991@gmail.com>
2024-12-18 19:27:47 +0530

    update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'database_development',
+    database: process.env.DB_NAME || 'Development',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
   },
