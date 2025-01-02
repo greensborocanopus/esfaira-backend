2a880d0d8f8a5b51b7956a443c418a4d26482554
murtuzakapadia121 <murtuza.kapadia121@gmail.com>
2025-01-02 15:46:04 +0530

    created apis for add, getall, and get single value

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const authRoutes = require('./authRoutes');
 const leagueRoutes = require('./leagueRoutes');
 const userRoutes = require('./userRoutes');
 const apiRouter = express.Router();
+const teamRoutes = require('./teamRoutes');
 
 // Define route prefixes
 apiRouter.use('/auth', authRoutes);
 apiRouter.use('/league', leagueRoutes);
 apiRouter.use('/user', userRoutes);
+apiRouter.use('/team', teamRoutes);
 
 module.exports = apiRouter;
