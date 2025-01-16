eac5c5b1344c300cb764e274145195b4b77bec20
canopus <vivekc19991@gmail.com>
2025-01-16 18:20:39 +0530

    update getAllUsers API

--- src/controllers/leagueController.js
+++ src/controllers/leagueController.js
@@ -166,17 +166,10 @@ const getSubleagues = async (req, res) => {
 
 const getLeagues = async (req, res) => {
   try {
-    let searchTerm = req.query.searchTerm;
-    // Fetch subleagues with associated leagues
-    const subleagues = await League.findAll({
-      where: {
-        league_name: {
-          [Op.like]: `%${searchTerm}%`, // Replace searchTerm with the actual search term
-        },
-      },
+    const leagues = await League.findAll({
+      //attributes: ['sub_league_name', 'venue_details', 'category', 'sub_league_id'], // Fetch specific Subleague fields
     });
-    // Transform the data into the desired format
-    res.status(200).json(subleagues);
+    return res.status(200).json(leagues);
   } catch (error) {
     console.error('Error fetching subleagues:', error);
     res.status(500).json({ message: 'Server error.' });
