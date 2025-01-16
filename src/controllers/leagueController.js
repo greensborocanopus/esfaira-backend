7c9c0953eb9771a8f7d5cf6a2e17f0e8a3e14347
canopus <vivekc19991@gmail.com>
2025-01-16 18:14:26 +0530

    update getJoinLeague API

--- src/controllers/leagueController.js
+++ src/controllers/leagueController.js
@@ -492,10 +492,8 @@ const getJoinLeague = async (req, res) => {
      }
 
     // Step 4: Send response
-    return res.status(200).json({
-      message: 'Joined leagues retrieved successfully.',
-      subLeagues,
-    });
+    return res.status(200).json(subLeagues);
+
   } catch (error) {
     console.error('Error fetching joined leagues:', error);
     return res.status(500).json({ message: 'Server error', error });
